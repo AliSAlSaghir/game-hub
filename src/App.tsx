83a5525f6440ed324a652f252870ebd8c6facbba
Ali AlSaghir <alisalsaghir@gmail.com>
2023-10-03 20:54:06 +0300

    Initializing the project and installing Shakra UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+import { Box, Button, Grid, List } from "@chakra-ui/react";
+import React from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Grid></Grid>;
+}
+
+export default App;
